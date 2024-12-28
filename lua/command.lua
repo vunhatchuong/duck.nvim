f36088d0645ae1d3177e5fec24751504b93879e1
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-12-28 09:32:11 +0700

    feat: Commands for cmdline

---
+++ lua/command.lua
@@ -0,0 +1,38 @@
+local duck = require("duck")
+
+local M = {}
+
+--- @param cmd string
+--- @param ... any
+---@return nil
+function M.load_command(cmd, ...)
+    if cmd == nil then
+        duck.hatch()
+        return
+    end
+
+    if cmd == "hatch" then
+        duck.hatch(...)
+    elseif cmd == "cook" then
+        duck.cook()
+    elseif cmd == "cook_all" then
+        duck.cook_all()
+    end
+end
+
+--- @param cmd string
+--- @return string[]
+function M.complete(cmd)
+    local options = { "hatch", "cook", "cook_all" }
+    local matches = {}
+
+    for i = 1, #options do
+        if options[i]:sub(1, #cmd) == cmd then
+            matches[#matches + 1] = options[i]
+        end
+    end
+
+    return matches
+end
+
+return M
---
+++ plugin/duck.lua
@@ -0,0 +1,8 @@
+local Command = require("command")
+
+vim.api.nvim_create_user_command("Duck", function(opts)
+    Command.load_command(unpack(opts.fargs))
+end, {
+    nargs = "*",
+    complete = Command.complete,
+})
